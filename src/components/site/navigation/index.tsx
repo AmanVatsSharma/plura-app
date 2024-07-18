e0f72ea6385a7139866c2434fba986cdf1f1e321
Aman Sharma <aman95026@gmail.com>
2024-07-18 15:03:59 +0000

    erros handled

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -12,8 +12,8 @@ type Props = {
 const Navigation = ({ user }: Props) => {
     return (
         <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
-        <aside className="flex items-center gap-2">
-        <Image
+            <aside className="flex items-center gap-2">
+                <Image
                     src={"/assets/plura-logo.svg"}
                     alt="logo"
                     width={40}
